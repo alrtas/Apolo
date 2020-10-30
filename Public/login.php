6105eff946166be6719f8b20f66687733072ef34
Thiago <alrrtas@gmail.com>
2020-10-30 00:07:33 -0300

    Criado front-end da pagina de login

---
+++ Public/login.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <style type="text/css">
+        html, body {
+            height: 100%;
+        }
+    </style>
+
+    <title>Login</title>
+  </head>
+  <body style="background-color: #2A2A2A">
+    <div class="h-100 d-flex justify-content-center">
+        <div class="col-md-9 col-sm-12 my-auto">
+            <div class="card text-center">
+                <div class="card-header">
+                    Apolo | Login
+                </div>
+                <div class="card-body align-middle">
+                    <form>
+                        <div class="form-inline justify-content-center mt-3">
+                            <label for="exampleInputEmail1">E-mail: </label>
+                            <input type="email" class="form-control ml-3" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="alrrtas@gmail.com">
+                        </div>
+                        <div class="form-inline justify-content-center mt-2">
+                            <label for="exampleInputPassword1">Senha: </label>
+                            <input type="password" class="form-control ml-3" id="exampleInputPassword1" placeholder="*********">
+                        </div>
+                        <button type="submit" class="btn btn-primary mt-4">Entrar</button>
+                        <button type="submit" class="btn btn-success mt-4">Criar conta</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  </body>
+</html>
