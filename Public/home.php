fa26979ae99ae8a38972f5df891bb1f775e745d8
Thiago Alberto da Silva <alrrtas@gmail.com>
2020-10-30 23:07:04 -0300

    Create home.php

---
+++ Public/home.php
@@ -0,0 +1,18 @@
+<?
+    include(dirname(__DIR__).DIRECTORY_SEPARATOR.join(DIRECTORY_SEPARATOR, array('Views','header.php' )));
+    setHeaders('Home');
+    include(dirname(__DIR__).DIRECTORY_SEPARATOR.join(DIRECTORY_SEPARATOR, array('Views','navbar.php' )));
+?>
+
+
+
+
+
+
+
+
+
+
+<?
+    include(dirname(__DIR__).DIRECTORY_SEPARATOR.join(DIRECTORY_SEPARATOR, array('Views','footer.php' )));
+?>
