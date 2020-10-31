ead4906ed0b932b36309b7c66c604af90ef1622d
Thiago Alberto da Silva <alrrtas@gmail.com>
2020-10-30 23:07:12 -0300

    Create navbar.php

---
+++ Views/navbar.php
@@ -0,0 +1,22 @@
+<?
+ $path = join(DIRECTORY_SEPARATOR, array('Public','favicon.png' ));
+ $iamgePath = dirname(__DIR__).DIRECTORY_SEPARATOR.$path;
+
+?>
+
+<nav class="navbar navbar-static-top" style="background-color: #cd422b;">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand text-white" href="#">Apolo Project</a>
+        </div>
+        <form class="form-inline">
+            <button class="btn btn-sm btn-outline-white" type="button">Smaller button</button>
+        </form>
+    </div>
+
+
+</nav>
+  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="js/bootstrap.min.js"></script>
