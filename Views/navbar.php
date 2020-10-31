605f4fe7543cad7cc6fa0921d6da58d5d6225952
Thiago Alberto da Silva <alrrtas@gmail.com>
2020-10-31 14:33:27 -0300

    Responsiva agora

--- Views/navbar.php
+++ Views/navbar.php
@@ -1,26 +1,53 @@
-<?
- $path = join(DIRECTORY_SEPARATOR, array('Public','favicon.png' ));
- $iamgePath = dirname(__DIR__).DIRECTORY_SEPARATOR.$path;
-
-?>
-
-<nav class="navbar navbar-light navbar-static-top navbar-expand-sm" style="background-color: #cd422b;">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand text-white" href="#">Apolo Project</a>
+<nav class="navbar navbar-light navbar-static-top navbar-expand-md w-100 " style="background-color: #cd422b;">
+    <div class="container"> 
+        <div class="col">
+            <a class="navbar-brand text-white" href="/home.php">Apolo Project</a>
         </div>
-        <form class="form-inline text-white">
-            <button class="btn btn-sm btn-outline-light ml-2" type="button"><b>COLABORADORES</b></button>
-            <button class="btn btn-sm btn-outline-light ml-2" type="button"><b>TREINAMENTOS</b></button>
-            <button class="btn btn-sm btn-outline-light ml-2" type="button"><b>TURMAS</b></button>
-            <button class="btn btn-sm btn-outline-light mx-2" type="button"><b>DASHBOARDS</b></button>
-            <button class="btn btn-sm btn-outline-dark ml-5" type="button"><b>SAIR</b></button>
-        </form>
+        <div class="col">
+            <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="col">
+        <div class="collapse navbar-collapse"  id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white mr-3" href="#" id="navbardrop" data-toggle="dropdown">
+                        COLABORADORES
+                    </a>
+                    <div class="dropdown-menu bg-dark">
+                        <a class="dropdown-item text-white" href="visualizar-colaboradores.php">Visualizar</a>
+                        <a class="dropdown-item text-white" href="criar-colaboradores.php">Criar</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white mr-3" href="#" id="navbardrop" data-toggle="dropdown">
+                        TREINAMENTOS
+                    </a>
+                    <div class="dropdown-menu  bg-dark">
+                        <a class="dropdown-item text-white" href="visualizar-treinamentos.php">Visualizar</a>
+                        <a class="dropdown-item text-white" href="criar-treinamentos.php">Criar</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white mr-3" href="#" id="navbardrop" data-toggle="dropdown">
+                        TURMAS
+                    </a>
+                    <div class="dropdown-menu bg-dark">
+                        <a class="dropdown-item text-white" href="visualizar-turmas.php">Visualizar</a>
+                        <a class="dropdown-item text-white" href="criar-turmas.php">Criar</a>
+                    </div>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link text-white mr-3" href="dashboard.php">DASHBOARDS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-3" href="logout.php"><b>SAIR</b></a>
+                </li>
+            </ul>
+        </div>
+        </div>
     </div>
-
-
 </nav>
-  
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-<script src="js/bootstrap.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
