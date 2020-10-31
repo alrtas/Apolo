44d624d9e06c0ddbc8da834b892912e8e8e8b042
Thiago Alberto da Silva <alrrtas@gmail.com>
2020-10-31 14:45:21 -0300

    Alterado frase principal

--- Public/home.php
+++ Public/home.php
@@ -7,7 +7,7 @@
         <div class="col-md-7 col-sm-9 my-auto">
             <div class="jumbotron text-center text-white rounded" style = "background-color: #2A2A2A"> 
                 <h1 class="display-4">Olá, Thiago!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p class="lead">Boa tarde, seja bem vindo! Seu nível de acesso é: <b>TREINADOR</b>.</p>
                 <hr class="my-4">
                 <a class="btn btn-dark btn-lg bg-dark" href="#" role="button">Institucional</a>
             </div>
