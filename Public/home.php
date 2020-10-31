6e8a7c4ca95231f183542dba229b473b86131cc3
Thiago Alberto da Silva <alrrtas@gmail.com>
2020-10-31 14:33:39 -0300

    Criado jumbotron

--- Public/home.php
+++ Public/home.php
@@ -3,12 +3,16 @@
     setHeaders('Home');
     include(dirname(__DIR__).DIRECTORY_SEPARATOR.join(DIRECTORY_SEPARATOR, array('Views','navbar.php' )));
 ?>
-
-
-
-
-
-
+    <div class="h-100 d-flex justify-content-center">
+        <div class="col-md-7 col-sm-9 my-auto">
+            <div class="jumbotron text-center text-white rounded" style = "background-color: #2A2A2A"> 
+                <h1 class="display-4">Olá, Thiago!</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4">
+                <a class="btn btn-dark btn-lg bg-dark" href="#" role="button">Institucional</a>
+            </div>
+        </div>
+    </div>
 
 
 
